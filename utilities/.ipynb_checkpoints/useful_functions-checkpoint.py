719ffe6471a3fd2b8fbd59fc681b036fa66036a7
skyecaplan <skyecaplan@gmail.com>
2025-08-28 20:37:32 +0000

    added useful functions

---
+++ utilities/.ipynb_checkpoints/useful_functions-checkpoint.py
@@ -0,0 +1,87 @@
+#  Generally useful functions for working with PACE data  #
+# Contribute as you see fit! And don't forget to comment! #
+
+import cartopy
+import glob 
+import xarray as xr 
+import numpy as np 
+import matplotlib.pyplot as plt 
+import cartopy.crs as ccrs
+import cf_xarray
+
+def open_nc(filename):
+    """ 
+    Opens a L1B- or L2-like PACE file and assign lat/lon as coordinates. 
+    Returns the entire observation_ or geophysical_data groups in the 
+        output dataset
+    Args:
+        filename - Path to input PACE file
+    Returns:
+        ds - xarray dataset with lat/lon as coords 
+    """
+    # Open file as datatree
+    dt = xr.open_datatree(filename)
+    if "L1B" in filename:
+        ds = dt["observation_data"].to_dataset()
+        ds.coords["longitude"] = dt["geolocation_data"]["longitude"]
+        ds.coords["latitude"] = dt["geolocation_data"]["latitude"]
+    elif "L2" in filename:
+        # First try to open the dataset with wavelength_3d as a coordinate
+        try:
+            ds = xr.merge(
+                (
+                dt.ds,
+                dt["geophysical_data"].to_dataset(),
+                dt["sensor_band_parameters"].coords,
+                dt["navigation_data"].ds.set_coords(("longitude", "latitude")).coords,
+                )
+            )
+        except:
+            # If that doesn't work, don't use sensor_band_params
+            ds = xr.merge(
+                (
+                dt.ds,
+                dt["geophysical_data"].to_dataset(),
+                dt["navigation_data"].ds.set_coords(("longitude", "latitude")).coords,
+                )
+            )
+    return ds
+
+def subset(ds, extent):
+    """
+    Subset a L2 PACE file
+    Skye's note: I don't think the Hackweek method using slicing works
+                 for L2's bc the lat/lon arrays are 2D, but please 
+                 correct me if I'm wrong!
+    Args:
+        ds - xarray dataset with lat/lon set as coordinates
+        extent - list of boundaries in [w, e, s, n] configuration
+    Returns:
+        sub - subset of ds clipped to boundaries
+    """
+    sub = ds.where(
+        (
+          (ds["latitude"]  > extent[2])
+        & (ds["latitude"]  < extent[3])
+        & (ds["longitude"] > extent[0])
+        & (ds["longitude"] < extent[1])
+        ),
+            drop=True,
+        )
+    return sub
+
+def mask_ds(ds, flag="CLDICE"):
+    """
+    Mask for a PACE dataset for an L2 flag using cf_xarray. Default is clouds
+    Args:
+        ds - xarray dataset containing "l2_flags" variable
+        flag - l2 flag to mask for (see https://oceancolor.gsfc.nasa.gov/resources/atbd/ocl2flags/)
+    Returns:
+        Masked dataset
+    """
+    # Check if cf_xarray recognizes l2_flags
+    if ds["l2_flags"].cf.is_flag_variable:
+        # If so, mask
+        return ds.where(~(ds["l2_flags"].cf == flag))
+    else:
+        print("l2flags not recognized as flag variable")
